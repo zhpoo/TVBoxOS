a2bf2356bfb55fd51355ccb9a7c2e03c50df783c
于俊 <215613905@qq.com>
2025-01-24 09:05:15 +0800

    兼容加载base64图片;简易修正个别直播配置加载失败的问题

--- app/src/main/java/com/github/tvbox/osc/util/live/TxtSubscribe.java
+++ app/src/main/java/com/github/tvbox/osc/util/live/TxtSubscribe.java
@@ -26,34 +26,37 @@ public class TxtSubscribe {
         ArrayList<String> urls;
         try {
             BufferedReader bufferedReader = new BufferedReader(new StringReader(str));
-            LinkedHashMap<String, ArrayList<String>> channel = new LinkedHashMap<>();
-            LinkedHashMap<String, ArrayList<String>> channelTemp = channel;
+            LinkedHashMap<String, ArrayList<String>> linkedHashMap2 = new LinkedHashMap<>();
+            LinkedHashMap<String, ArrayList<String>> channelTemp = linkedHashMap2;
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 if (line.equals("")) continue;
                 if (line.startsWith("#EXTM3U")) continue;
-                if (line.startsWith("#EXTINF")) {
+                if (isSetting(line)) continue;
+                if (line.startsWith("#EXTINF") || line.contains("#EXTINF")) {
                     String name = getStrByRegex(NAME_PATTERN, line);
                     String group = getStrByRegex(GROUP_PATTERN, line);
                     String url = bufferedReader.readLine().trim();
-                    if (linkedHashMap.containsKey(group)) {
-                        channelTemp = linkedHashMap.get(group);
-                    } else {
-                        channelTemp = new LinkedHashMap<>();
-                        linkedHashMap.put(group, channelTemp);
+                    if (isUrl(url)) {
+                        if (linkedHashMap.containsKey(group)) {
+                            channelTemp = linkedHashMap.get(group);
+                        } else {
+                            channelTemp = new LinkedHashMap<>();
+                            linkedHashMap.put(group, channelTemp);
+                        }
+                        if (null != channelTemp && channelTemp.containsKey(name)) {
+                            urls = channelTemp.get(name);
+                        } else {
+                            urls = new ArrayList<>();
+                            channelTemp.put(name, urls);
+                        }
+                        if (null != urls && !urls.contains(url)) urls.add(url);
                     }
-                    if (null != channelTemp && channelTemp.containsKey(name)) {
-                        urls = channelTemp.get(name);
-                    } else {
-                        urls = new ArrayList<>();
-                        channelTemp.put(name, urls);
-                    }
-                    if (null != urls && !urls.contains(url)) urls.add(url);
                 }
             }
             bufferedReader.close();
-            if (channel.isEmpty()) return;
-            linkedHashMap.put("未分组", channel);
+            if (linkedHashMap2.isEmpty()) return;
+            linkedHashMap.put("未分组", linkedHashMap2);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,6 +65,14 @@ public class TxtSubscribe {
         Matcher matcher = pattern.matcher(line);
         if (matcher.find()) return matcher.group(1);
         return pattern.pattern().equals(GROUP_PATTERN.pattern()) ? "未分组" : "未命名";
+    }
+
+    private static boolean isUrl(String url) {
+        return !url.isEmpty() && (url.startsWith("http") || url.startsWith("rtp") || url.startsWith("rtsp") || url.startsWith("rtmp"));
+    }
+
+    private static boolean isSetting(String line) {
+        return line.startsWith("ua") || line.startsWith("parse") || line.startsWith("click") || line.startsWith("player") || line.startsWith("header") || line.startsWith("format") || line.startsWith("origin") || line.startsWith("referer") || line.startsWith("#EXTHTTP:") || line.startsWith("#EXTVLCOPT:") || line.startsWith("#KODIPROP:");
     }
 
     public static void parseTxt(LinkedHashMap<String, LinkedHashMap<String, ArrayList<String>>> linkedHashMap, String str) {
@@ -91,7 +102,7 @@ public class TxtSubscribe {
                             String trim2 = split[0].trim();
                             for (String str2 : split[1].trim().split("#")) {
                                 String trim3 = str2.trim();
-                                if (!trim3.isEmpty() && (trim3.startsWith("http") || trim3.startsWith("rtp") || trim3.startsWith("rtsp") || trim3.startsWith("rtmp"))) {
+                                if (isUrl(trim3)) {
                                     if (!linkedHashMap3.containsKey(trim2)) {
                                         arrayList = new ArrayList<>();
                                         linkedHashMap3.put(trim2, arrayList);
