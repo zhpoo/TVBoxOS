c323f545e7222d726c71ad98e9e406b388388bee
于俊 <215613905@qq.com>
2025-02-21 16:42:42 +0800

    暂时去除base64图片兼容

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
@@ -1,9 +1,6 @@
 package com.github.tvbox.osc.ui.adapter;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.text.TextUtils;
-import android.util.Base64;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -15,7 +12,6 @@ import com.github.tvbox.osc.R;
 import com.github.tvbox.osc.api.ApiConfig;
 import com.github.tvbox.osc.bean.Movie;
 import com.github.tvbox.osc.picasso.RoundTransformation;
-import com.github.tvbox.osc.util.Base64Img;
 import com.github.tvbox.osc.util.DefaultConfig;
 import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.MD5;
@@ -63,21 +59,16 @@ public class HomeHotVodAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHol
         //由于部分电视机使用glide报错
         if (!TextUtils.isEmpty(item.pic)) {
             item.pic=item.pic.trim();
-            if(Base64Img.isBase64Image(item.pic)){
-                // 如果是 Base64 图片，解码并设置
-                ivThumb.setImageBitmap(Base64Img.decodeBase64ToBitmap(item.pic));
-            }else {
-                Picasso.get()
-                        .load(DefaultConfig.checkReplaceProxy(item.pic))
-                        .transform(new RoundTransformation(MD5.string2MD5(item.pic))
-                                .centerCorp(true)
-                                .override(AutoSizeUtils.mm2px(mContext, 300), AutoSizeUtils.mm2px(mContext, 400))
-                                .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
-                        .placeholder(R.drawable.img_loading_placeholder)
-                        .noFade()
-                        .error(R.drawable.img_loading_placeholder)
-                        .into(ivThumb);
-            }
+            Picasso.get()
+                    .load(DefaultConfig.checkReplaceProxy(item.pic))
+                    .transform(new RoundTransformation(MD5.string2MD5(item.pic))
+                            .centerCorp(true)
+                            .override(AutoSizeUtils.mm2px(mContext, 300), AutoSizeUtils.mm2px(mContext, 400))
+                            .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
+                    .placeholder(R.drawable.img_loading_placeholder)
+                    .noFade()
+                    .error(R.drawable.img_loading_placeholder)
+                    .into(ivThumb);
         } else {
             ivThumb.setImageResource(R.drawable.img_loading_placeholder);
         }
